4b68bc0853808f206d16d4a0e4fd47e48cbe17d6
Moyo Olojede <Moyo@Moyos-MBP.home>
2021-05-25 14:38:54 -0400

    Changes to problem 5

--- function-problems/Problem-5.js
+++ function-problems/Problem-5.js
@@ -2,21 +2,24 @@
 But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
 For numbers that are multiples of both three and five print "FizzBuzz".
 
-Step 1: Make an array and apply a length of 100.
-Step 2: Apply a map method and make a function with val and index as parameters.
+Step 1: Make an variable called i.
+Step 2: Make a loop through the variable i.
 Step 3: Add if statement showing multiples of 5 and 3 returning FizzBuzz.
-Step 4: Add if statement showing multiples of and 3 returning Fizz.
-Step 5: Add if statement showing multiples of 5 returning Buzz
-Step 6: return index.
-Step 7: Make the array a string.
+Step 4: Add a else if statement showing multiples of and 3 returning Fizz.
+Step 5: Add a else if statement showing multiples of 5 returning Buzz
+Step 6: console.log i.
 */
 
-console.log(
-  Array.apply(null, {length: 100}).map(function(val, index) {
-    index++;
-    if (index % 15 == 0){return "FizzBuzz";}
-    if (index % 3 == 0){return "Fizz";}
-    if (index % 5 == 0){return "Buzz";}
-    return index;
-  }).join()
-);
+var i;
+for( i = 1; i <= 100; i++){
+  if (  i % 15 == 0) {
+   console.log('Fizz Buzz')
+    }else if (i % 5 == 0) {
+      console.log('buzz');
+    }else if (i % 3 == 0) {
+      console.log('Fizz');
+    }else {
+    console.log(i);
+    }
+}
+
