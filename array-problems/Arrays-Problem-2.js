af07339f2f215db87f3a2ae5e75377b3bfe5179d
Moyo Olojede <Moyo@Moyos-MBP.home>
2021-06-01 17:43:47 -0400

    Added arrays problem 2

---
+++ array-problems/Arrays-Problem-2.js
@@ -0,0 +1,9 @@
+/* Write a simple JavaScript program to join all elements of the 
+following array into a string. 
+Step 1: Make an array called myColor.
+Step 2: Join the array into a string.
+ */
+
+var myColor=["Red", "Green", "White", "Black"];
+var newColors = myColor.join()
+console.log(newColors);
