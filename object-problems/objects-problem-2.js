3c9d189df5872b7605d75bcb55e5eecbc4b9badc
Moyo Olojede <Moyo@Moyos-MBP.home>
2021-06-01 19:29:59 -0400

    object problem 2 added

---
+++ object-problems/objects-problem-2.js
@@ -0,0 +1,22 @@
+/*Create an array of people objects with first name, last name, and age, 
+then write a JavaScript program to display the first and last names of all the people.
+*/
+var people =[ 
+    {
+      firstName: "Moyo",
+      lastName: "Olojede",
+      age: 30,
+    },
+    {
+      firstName: "Anthony",
+      lastName: "Roberts",
+      age: 29,
+    },
+    {
+      firstName: "Justin",
+      lastName: "Thompson",
+      age: 30,
+    },
+]
+console.log(people);
+
