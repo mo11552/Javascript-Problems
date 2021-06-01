5b84e74a6cbc932cb4e3dbe219160815c6803d41
Moyo Olojede <Moyo@Moyos-MBP.home>
2021-06-01 17:55:47 -0400

    array problem 3 added

---
+++ array-problems/Arrays-Problem-3.js
@@ -0,0 +1,9 @@
+/* Write a JavaScript program to sort the items of an array.
+Step 1: Make an array called myArray.
+Step 2: sort myArray and make it a new array called newArray.
+*/
+
+
+var myArray = [-3,8,7,6,5,-4,3,2,1];
+var newArray = myArray.sort();
+console.log(newArray);
