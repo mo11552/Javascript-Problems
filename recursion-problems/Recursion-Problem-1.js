8fc39b74c7eb1416082e03f20da20c2bd67e17bb
Moyo Olojede <Moyo@Moyos-MBP.home>
2021-06-01 21:15:17 -0400

    recursion problem 1 added

---
+++ recursion-problems/Recursion-Problem-1.js
@@ -0,0 +1,10 @@
+/*Write a JavaScript function to calculate the factorial of a number.*/
+
+function factorial(n){
+    if(n == 0 || n == 1){
+        return 1;
+    }else{
+        return n * factorial(n-1);
+    }
+}
+console.log(factorial(3));
