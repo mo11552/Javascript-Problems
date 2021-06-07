b14a4fa6e7fbb6a24a6978939ab8ea31e59fa346
Moyo Olojede <Moyo@Moyos-MBP.home>
2021-06-07 14:59:54 -0400

    Changes made to objects problem 2

--- object-problems/objects-problem-2.js
+++ object-problems/objects-problem-2.js
@@ -1,7 +1,7 @@
 /*Create an array of people objects with first name, last name, and age, 
 then write a JavaScript program to display the first and last names of all the people.
 Step 1: Make an array of objects called people.
-Step 2: Make a function called listOfNames that has 2 parameters called obj and key.
+Step 2: Make a function called listOfNames that has 2 parameters called arr and key.
 Step 3: Make a if statement that deletes the key age from the array of objects.
 Step 4: return people.
 */
@@ -23,11 +23,13 @@ var people =[
     },
 ]
 
-function listOfNames(obj, key) {
-  if(1 == 1) {
-    delete people.age;
-    return people
-  }
-};
+ function listOfNames(arr, key) {
+   if(1 == 1) {
+     delete people[0].age;
+     delete people[1].age;
+     delete people[2].age;
+     return people
+   }
+ };
 console.log(listOfNames(people));
 
